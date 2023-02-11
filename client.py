e1fa96f27b8f9a7673c3ede2aeb048e551db7e3d
jonathan YHWH <alphajorider@gmail.com>
2023-02-11 21:18:06 +0100

    thread

--- client.py
+++ client.py
@@ -1,5 +1,18 @@
 import socket
+from threading import Thread
 
+def Send(client):
+    while True:
+        msg = input("->")
+        msg = msg.encode("utf-8")
+        socket.send(msg)
+
+def Receive(client):
+    while True:
+        requete_server = socket.recv(500)
+        requete_server = requete_server.decode("utf-8")
+        print(requete_server)
+        
 host = "192.168.0.25"
 port = 6390
 
@@ -8,12 +21,8 @@ socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 socket.connect((host, port))
 
-while True:
-    msg = input("->")
-    msg = msg.encode("utf-8")
-    socket.send(msg)
-    
-    requete_server = socket.recv(500)
-    requete_server = requete_server.decode("utf-8")
-    print(requete_server)
-    
+envoi = Thread(target=Send,args=[client])
+reception = Thread(target=Receive,args=[client])
+
+envoi.start()
+reception.start()
--- server.py
+++ server.py
@@ -1,5 +1,21 @@
 import socket
+from threading import Thread
 
+def Send(client):
+    while True:
+        msg = input("->")
+        msg = msg.decode("utf-8")
+        client.send(msg)
+
+def Receive(client):
+    while True:
+        requete_client = client.recv(500)
+        requete_client = requete_client.decode("utf-8")
+        print(requete_client)
+        if not requete_client: #Si on perd la connexion
+            print("le client s'est déconnecté")
+            break
+        
 host = "192.168.0.25"
 port = 6390
 
@@ -13,17 +29,14 @@ socket.listen(1)
 client, ip = socket.accept()
 print("le client :",ip,"s'est connecté")
 
-while True:
-    requete_client = client.recv(500)
-    requete_client = requete_client.decode("utf-8")
-    print(requete_client)
-    if not requete_client: #Si on perd la connexion
-        print("le client s'est déconnecté")
-        break
-    msg = input("->")
-    msg = msg.decode("utf-8")
-    client.send(msg)
-    
+envoi = Thread(target=Send,args=[client])
+reception = Thread(target=Receive,args=[client])
+
+envoi.start()
+reception.start()
+
+reception.join()
+
 client.close()
 socket.close()
     
